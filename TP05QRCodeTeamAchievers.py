2426caf4536ebef35080d4b5488035a774243fd0
NILESH SARKAR <167231633+nileshsarkarRA@users.noreply.github.com>
2024-08-18 14:42:04 +0530

    Update TP05QRCodeTeamAchievers.py

--- TP05QRCodeTeamAchievers.py
+++ TP05QRCodeTeamAchievers.py
@@ -7,26 +7,35 @@ print("""
 
 ## Import all relevant modules includeing a redirect urlopener called urlopen and a custom QRCode maker sengo
 
+## QR code 
+# QR Code for Redirecting to Any website
+# Caution
+print("""
+!! This QR Code doesnt redirect when I used Google Lens. I think its due to the background image quality. Using other QR code scanners works however !!
+""")
+
+## Import all relevant modules includeing a redirect urlopener called urlopen and a custom QRCode maker sengo
+
 import segno
 from urllib.request import urlopen
 
 #QR Code File name
 
-png_file_name="TP05TeamAchieversQRCode.png"
+png_file_name="QRCode.png"
 
 ## assign website logo and address relevant variables for opening site
-website_address = segno.make_qr("https://amitabhthakur856.wixsite.com/humanscarefoundation")
-website_logo = urlopen("https://static.wixstatic.com/media/d6cc27_a0de5c40d82b4176bf732139363efda7~mv2.png/v1/fill/w_448,h_370,al_c,lg_1,q_85,enc_auto/20221206_004234_0000.png")
+website_address = segno.make_qr("https://github.com/nileshsarkarRA")
+website_logo = urlopen("https://logos-world.net/wp-content/uploads/2020/11/GitHub-Logo.png")
 
 # modification to create a custom QRcode
 website_address.to_artistic(
     background=website_logo,
-    target="TP05TeamAchieversQRCode.png",
+    target="QRCode.png",
     scale=15,
-    light = "lightgreen",
-    dark = "green",
+    light = "lightblue",
+    dark = "white",
     border = 1,
     quiet_zone = "lightgreen"
 )
 # Display File name and success of operations
-print(f"\nExecuted Successfully!! Find the QR Code named {png_file_name} to redirect to Humans Care Foundation Website.\n")
+print(f"\nExecuted Successfully!! Find the QR Code named {png_file_name}.\n")
