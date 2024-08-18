7e4ba4c118002b2e5b15d3e15bc8d089f9fe5686
NILESH SARKAR <167231633+nileshsarkarRA@users.noreply.github.com>
2024-08-18 14:42:29 +0530

    Update TP05QRCode.py

--- TP05QRCode.py
+++ TP05QRCode.py
@@ -14,21 +14,35 @@ qrcode_rotated = qrcode.to_pil(
 ).rotate(45, expand=True)
 qrcode_rotated.save("TP05QRCode.png")"""
 
-# QR Code for Redirecting to HCF Website
+## QR code 
+# QR Code for Redirecting to Any website
+# Caution
+print("""
+!! This QR Code doesnt redirect when I used Google Lens. I think its due to the background image quality. Using other QR code scanners works however !!
+""")
+
+## Import all relevant modules includeing a redirect urlopener called urlopen and a custom QRCode maker sengo
+
 import segno
 from urllib.request import urlopen
 
-png_file_name="TP05TeamAchieversQRCode.png"
+#QR Code File name
 
-website_address = segno.make_qr("https://amitabhthakur856.wixsite.com/humanscarefoundation")
-site_url = urlopen("https://static.wixstatic.com/media/d6cc27_a0de5c40d82b4176bf732139363efda7~mv2.png/v1/fill/w_448,h_370,al_c,lg_1,q_85,enc_auto/20221206_004234_0000.png")
+png_file_name="QRCode.png"
+
+## assign website logo and address relevant variables for opening site
+website_address = segno.make_qr("https://github.com/nileshsarkarRA")
+website_logo = urlopen("https://logos-world.net/wp-content/uploads/2020/11/GitHub-Logo.png")
+
+# modification to create a custom QRcode
 website_address.to_artistic(
-    background=site_url,
-    target="TP05TeamAchieversQRCode.png",
+    background=website_logo,
+    target="QRCode.png",
     scale=15,
-    light = "lightgreen",
-    dark = "green",
+    light = "lightblue",
+    dark = "white",
     border = 1,
     quiet_zone = "lightgreen"
 )
-print(f"\nExecuted Successfully!! Find the QR Code named {png_file_name}\n")
+# Display File name and success of operations
+print(f"\nExecuted Successfully!! Find the QR Code named {png_file_name}.\n")
